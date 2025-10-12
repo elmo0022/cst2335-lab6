89150a66321ce043b1cec312dce60aedec9e838f
Your Full Name <you@example.com>
2025-10-12 14:11:48 -0400

    Step 8: Add French localization

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.androidlabs;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
-import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
     private List<TodoItem> todoList;
@@ -40,6 +41,31 @@ public class MainActivity extends AppCompatActivity {
                 addTodoItem();
             }
         });
+
+        // Long click listener for deletion (CORRECTED PLACEMENT)
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showDeleteDialog(position);
+                return true;
+            }
+        });
+    }
+
+    private void showDeleteDialog(final int position) {
+        new AlertDialog.Builder(this)
+                .setTitle("Do you want to delete this?")
+                .setMessage("The selected row is: " + position)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        todoList.remove(position);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "Item deleted", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     private void addTodoItem() {
